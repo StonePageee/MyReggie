5221c76eee70ef931677ff554d25d429f97be182
StonePageee <l15369256221@163.com>
2023-04-01 16:22:01 +0800

    使用Redis缓存，实现分类展示栏目的缓存操作

--- src/main/java/com/lts/controller/CategoryController.java
+++ src/main/java/com/lts/controller/CategoryController.java
@@ -6,6 +6,7 @@ import com.lts.common.R;
 import com.lts.entity.Category;
 import com.lts.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,19 +18,26 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     /**
      * 添加菜品分类或套餐分类信息
+     *
      * @param category
      * @return
      */
     @PostMapping
     public R<String> save(@RequestBody Category category) {
         categoryService.save(category);
+        String key = "categoryList";
+        redisTemplate.delete(key);
         return R.success("添加成功！");
     }
 
     /**
      * 查询所有分类信息
+     *
      * @param page
      * @param pageSize
      * @return
@@ -48,40 +56,56 @@ public class CategoryController {
 
     /**
      * 修改分类信息
+     *
      * @param category
      * @return
      */
     @PutMapping
-    public R<String> update(@RequestBody Category category){
+    public R<String> update(@RequestBody Category category) {
         categoryService.updateById(category);
+        String key = "categoryList";
+        redisTemplate.delete(key);
         return R.success("修改成功！");
     }
 
     /**
      * 根据id删除分类
+     *
      * @param ids
      * @return
      */
     @DeleteMapping
-    public R<String> delete(Long ids){
+    public R<String> delete(Long ids) {
         categoryService.remove(ids);
+
         return R.success("删除成功");
     }
 
     /**
      * 根据类型返回目标分类集合
+     *
      * @param category
      * @return
      */
     @GetMapping("/list")
-    public R<List<Category>> list(Category category){
+    public R<List<Category>> list(Category category) {
+
+        List<Category> categoryList;
+        String key = "categoryList";
+
+//      先从redis中获取数据
+        categoryList = (List<Category>) redisTemplate.opsForValue().get(key);
+        if (categoryList != null) {
+            return R.success(categoryList);
+        }
 
         LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(category.getType() != null ,Category::getType,category.getType());
+        queryWrapper.eq(category.getType() != null, Category::getType, category.getType());
         queryWrapper.orderByAsc(Category::getSort).orderByDesc(Category::getUpdateTime);
 
-        List<Category> categoryList = categoryService.list(queryWrapper);
+        categoryList = categoryService.list(queryWrapper);
+        redisTemplate.opsForValue().set(key, categoryList);
+
         return R.success(categoryList);
     }
-
 }
--- src/main/java/com/lts/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/lts/service/impl/CategoryServiceImpl.java
@@ -11,7 +11,11 @@ import com.lts.service.CategoryService;
 import com.lts.service.DishService;
 import com.lts.service.SetmealService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
@@ -22,24 +26,40 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> i
     @Autowired
     private SetmealService setmealService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Override
     public void remove(Long id) {
+
         LambdaQueryWrapper<Dish> dishLambdaQueryWrapper = new LambdaQueryWrapper<>();
-        dishLambdaQueryWrapper.eq(Dish::getCategoryId,id);
+        dishLambdaQueryWrapper.eq(Dish::getCategoryId, id);
         int dishCount = dishService.count(dishLambdaQueryWrapper);
 
         LambdaQueryWrapper<Setmeal> setmealLambdaQueryWrapper = new LambdaQueryWrapper<>();
-        setmealLambdaQueryWrapper.eq(Setmeal::getCategoryId,id);
+        setmealLambdaQueryWrapper.eq(Setmeal::getCategoryId, id);
         int setmealCount = setmealService.count(setmealLambdaQueryWrapper);
 
-        if (dishCount > 0){
+        Category category = this.getById(id);
+        Integer type = category.getType();
+
+        Collection<String> key = new ArrayList<>();
+        key.add("categoryList");
+        if (type == 1) {
+            key.add("category_" + id + "_1");
+        }else {
+            key.add("category_setmeal" + id);
+        }
+
+        if (dishCount > 0) {
             throw new CustomException("删除失败，当前分类下已关联菜品");
         }
 
-        if (setmealCount > 0){
+        if (setmealCount > 0) {
             throw new CustomException("删除失败，当前分类下已关联套餐");
         }
 
-        super.removeById(id);
+        this.removeById(id);
+        redisTemplate.delete(key);
     }
 }
